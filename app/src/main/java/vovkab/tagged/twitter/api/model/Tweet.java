4bb10ed1d34dad77e530e140ee1096edcb55b7bd
Vladimir Baryshnikov <vovkab@gmail.com>
2014-09-17 14:20:23 -0700

    Make Tweet and User implement parcelable

--- app/src/main/java/vovkab/tagged/twitter/api/model/Tweet.java
+++ app/src/main/java/vovkab/tagged/twitter/api/model/Tweet.java
@@ -1,8 +1,11 @@
 package vovkab.tagged.twitter.api.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Tweet {
+public class Tweet implements Parcelable {
     public final String id;
     public final String text;
     public final User user;
@@ -13,5 +16,28 @@ public class Tweet {
         this.id = id;
         this.text = text;
         this.user = user;
+    }
+
+    public static Creator<Tweet> CREATOR = new Creator<Tweet>() {
+        @Override public Tweet createFromParcel(Parcel source) {
+            String id = source.readString();
+            String text = source.readString();
+            User user = source.readParcelable(User.class.getClassLoader());
+            return new Tweet(id, text, user);
+        }
+
+        @Override public Tweet[] newArray(int size) {
+            return new Tweet[size];
+        }
+    };
+
+    @Override public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(text);
+        dest.writeParcelable(user, 0);
+    }
+
+    @Override public int describeContents() {
+        return 0;
     }
 }
--- app/src/main/java/vovkab/tagged/twitter/api/model/User.java
+++ app/src/main/java/vovkab/tagged/twitter/api/model/User.java
@@ -1,10 +1,13 @@
 package vovkab.tagged.twitter.api.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import vovkab.tagged.twitter.api.helper.ImageHelper;
 
-public class User {
+public class User implements Parcelable {
     public final String name;
     public final String screenName;
     public final String profileImageUrl;
@@ -17,5 +20,28 @@ public class User {
         this.screenName = screenName;
         this.profileImageUrl = profileImageUrl;
         this.profileImageBiggerUrl = ImageHelper.profileBigger(profileImageUrl);
+    }
+
+    public static Creator<User> CREATOR = new Creator<User>() {
+        @Override public User createFromParcel(Parcel source) {
+            String name = source.readString();
+            String screenName = source.readString();
+            String profileImageUrl = source.readString();
+            return new User(name, screenName, profileImageUrl);
+        }
+
+        @Override public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    @Override public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(screenName);
+        dest.writeString(profileImageUrl);
+    }
+
+    @Override public int describeContents() {
+        return 0;
     }
 }
